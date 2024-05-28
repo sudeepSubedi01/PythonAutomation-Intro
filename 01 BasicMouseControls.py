ddde00d041d5a04e7b80b48c11c8da972732ba5f
Sudip Subedi <sudip.subedi99@gmail.com>
2024-05-28 15:48:29 +0545

    basic mouse controls

---
+++ 01 BasicMouseControls.py
@@ -0,0 +1,26 @@
+import pyautogui as pg
+import time
+
+print(pg.size())
+
+# pg.moveTo(100,100,duration=1)
+
+# pg.moveRel(0,50,duration=2)
+
+# print(pg.position())
+
+# pg.click(70,20,duration=1)
+
+# time.sleep(10)
+
+#make a rectangle
+# pg.moveTo(960,540,duration=1)
+# pg.dragRel(0,50,duration=1)
+# pg.dragRel(50,0,duration=1)
+# pg.dragRel(0,-50,duration=1)
+# pg.dragRel(-50,0,duration=1)
+
+#scroll
+time.sleep(3)
+pg.scroll(-500)
+pg.scroll(200)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+print(pg.size())
+
+# pg.moveTo(100,100,duration=1)
+
+# pg.moveRel(0,50,duration=2)
+
+# print(pg.position())
