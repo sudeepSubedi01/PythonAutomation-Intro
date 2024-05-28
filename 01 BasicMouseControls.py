6edf56ca0ace3d08951088c01369b376095c5b7a
Sudip Subedi <sudip.subedi99@gmail.com>
2024-05-28 16:15:45 +0545

    basic keyboard

--- 01 BasicMouseControls.py
+++ 01 BasicMouseControls.py
@@ -2,25 +2,20 @@ import pyautogui as pg
 import time
 
 print(pg.size())
+pg.moveTo(100,100,duration=1,tween=pg.easeInOutQuad)
+pg.moveRel(0,50,duration=2)
+print(pg.position())
+pg.click(70,20,duration=1)
+time.sleep(3)
 
-# pg.moveTo(100,100,duration=1)
+# make a rectangle
+pg.moveTo(960,540,duration=1)
+pg.dragRel(0,50,duration=1)
+pg.dragRel(50,0,duration=1)
+pg.dragRel(0,-50,duration=1)
+pg.dragRel(-50,0,duration=1)
 
-# pg.moveRel(0,50,duration=2)
-
-# print(pg.position())
-
-# pg.click(70,20,duration=1)
-
-# time.sleep(10)
-
-#make a rectangle
-# pg.moveTo(960,540,duration=1)
-# pg.dragRel(0,50,duration=1)
-# pg.dragRel(50,0,duration=1)
-# pg.dragRel(0,-50,duration=1)
-# pg.dragRel(-50,0,duration=1)
-
-#scroll
+# scroll
 time.sleep(3)
 pg.scroll(-500)
 pg.scroll(200)
--- 02 BasicKeyboard.py
+++ 02 BasicKeyboard.py
@@ -1,8 +1,19 @@
-I lets your Python scripts control the mouse and keyboard to automate interactions with other applications. The API is designed to be simple. PyAutoGUI works on Windows, macOS, and Linux, and runs on Python 2 and 3.
+import pyautogui as pg
+
+print(pg.position())
+# pg.click(1397,79,duration=3)
+# pg.typewrite('Hello World')
+# pg.write('hi world', interval=1)
+# pg.hotkey('ctrlleft','a')
+# pg.hotkey('altleft','up')
+# pg.press('hhh')
+
+pg.click(41,122,duration=5)
+pg.write(f'''PyAutoGUI lets your Python scripts control the mouse and keyboard to automate interactions with other applications. The API is designed to be simple. PyAutoGUI works on Windows, macOS, and Linux, and runs on Python 2 and 3.
 To install with pip, run pip install pyautogui. See the Installation page for more details.
 The source code is available on: https://github.com/asweigart/pyautogui
 PyAutoGUI has several features:Moving the mouse and clicking in the windows of other applications.
 Sending keystrokes to applications (for example, to fill out forms).
 Take screenshots, and given an image (for example, of a button or checkbox), and find it on the screen.
 Locate an application’s window, and move, resize, maximize, minimize, or close it (Windows-only, currently).
-Display alert and message boxes.''')
+Display alert and message boxes.''',interval=0.01)
